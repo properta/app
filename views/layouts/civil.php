460eefbc2c408be63c41abce27ea4274dd9f1a5b
Imron Rosadi <imronrosadiee@gmail.com>
2022-08-03 04:48:25 +0700

    ok

--- views/layouts/civil.php
+++ views/layouts/civil.php
@@ -33,41 +33,34 @@ $breadcrumb = BC::generateBreadcrumbs($router, "breadcrumb-item");
             <nav class="navbar navbar-expand-lg main-navbar" style="z-index:unset">
                 <form class="form-inline mr-auto">
                     <ul class="navbar-nav mr-3">
-                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
-                                    class="fas fa-bars"></i></a></li>
-                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
-                                    class="fas fa-search"></i></a></li>
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
                     </ul>
                     <div class="form-group col-6">
                         <select class="form-control select2 get-projects">
                             <?php if (Yii::$app->session->get('id')) : ?>
-                            <option selected value="<?= Yii::$app->session->get('id') ?>">
-                                <?= Yii::$app->session->get('text') ?>
-                            </option>
+                                <option selected value="<?= Yii::$app->session->get('id') ?>">
+                                    <?= Yii::$app->session->get('text') ?>
+                                </option>
                             <?php else : ?>
-                            <option>--Choose Projects--</option>
+                                <option>--Choose Projects--</option>
                             <?php endif; ?>
                         </select>
                     </div>
                 </form>
                 <ul class="navbar-nav navbar-right">
-                    <li class="dropdown"><a href="#" data-toggle="dropdown"
-                            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                            <img alt="image"
-                                src="<?= Yii::$app->user->identity->image ?? Yii::$app->homeUrl . "theme/stisla/assets/img/avatar/avatar-1.png" ?>"
-                                class="rounded-circle profile-widget-picture mt-n3" style="width:32px; height:32px">
+                    <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                            <img alt="image" src="<?= Yii::$app->user->identity->image ?? Yii::$app->homeUrl . "theme/stisla/assets/img/avatar/avatar-1.png" ?>" class="rounded-circle profile-widget-picture mt-n3" style="width:32px; height:32px">
                             <div class="d-sm-none d-lg-inline-block">Hi,
                                 <?= Yii::$app->user->identity->full_name ?? "Annonym" ?></div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
-                            <a href="<?= \yii\helpers\Url::to('administrator/profiles/me', true) ?>"
-                                class="dropdown-item has-icon">
+                            <a href="<?= \yii\helpers\Url::to('administrator/profiles/me', true) ?>" class="dropdown-item has-icon">
                                 <i class="far fa-user"></i> Profile
                             </a>
                             <!--                            </a>-->
                             <div class="dropdown-divider"></div>
-                            <a href="<?= \yii\helpers\Url::to('site/logout', true) ?>" data-method="post"
-                                class="dropdown-item has-icon text-danger">
+                            <a href="<?= \yii\helpers\Url::to('site/logout', true) ?>" data-method="post" class="dropdown-item has-icon text-danger">
                                 <i class="fas fa-sign-out-alt"></i> Logout
                             </a>
                         </div>
@@ -77,7 +70,7 @@ $breadcrumb = BC::generateBreadcrumbs($router, "breadcrumb-item");
             <div class="main-sidebar">
                 <aside id="sidebar-wrapper">
                     <div class="sidebar-brand">
-                        <a href="index.html">Civil</a>
+                        <a href="index.html">Civil App</a>
                     </div>
                     <div class="sidebar-brand sidebar-brand-sm">
                         <a href="index.html">ADM</a>
